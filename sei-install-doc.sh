c2206fdfa9f8394c63c8b78458512ab5d0262937
Laércio de Sousa <lbsousajr@gmail.com>
2012-07-05 14:43:10 -0300

    Imported Upstream version 2.12.1.1+update1

---
+++ sei-install-doc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+. linux/share/functions
+. /etc/lsb-release
+
+case "${DISTRIB_CODENAME}" in
+    "hardy")
+        MKTEMP_OPTS="-t"
+    ;;
+
+    *)
+        MKTEMP_OPTS="--tmpdir"
+    ;;
+esac
+
+download_dir=${download_dir:-`mktemp -d ${MKTEMP_OPTS} sei-download-XXXXXX`}
+unpack_dir=${unpack_dir:-`mktemp -d ${MKTEMP_OPTS} sei-unpack-XXXXXX`}
+orig_dir=${unpack_dir}/usr/share/pte-pmmc/menu_sei/manual
+dest_dir=${1}
+
+sei_download ${download_dir}
+sei_unpack ${download_dir} ${unpack_dir}
+sei_install_doc ${orig_dir} ${dest_dir} && rm -rf ${download_dir} ${unpack_dir}
